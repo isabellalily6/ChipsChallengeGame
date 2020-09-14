c0d6e6ed881c44aab4a8ae01ad68579c03e00aa1
doornbbenj <doornbbenj@myvuw.ac.nz>
2020-09-14 13:02:18 +1200

    made imageURl method

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Tile.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Tile.java
@@ -1,7 +1,25 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
-public interface Tile {
+/**
+ * Tile class declares all of the required fields and methods for any tile on the maze.
+ * @author Benjamin Doornbos 300487256
+ */
+public abstract class Tile {
+    private String imageURl;
 
-    public String getImageURL();
+    private int row;
+    private int col;
 
+
+    public String getImageURl() {
+        return imageURl;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
 }
--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Treasure.java
+++
@@ -1,8 +0,0 @@
-package nz.ac.vuw.ecs.swen225.gp20.maze;
-
-public class Treasure implements Tile {
-    @Override
-    public String getImageURL() {
-        return null;
-    }
-}
