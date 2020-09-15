2e102525db3d4252df0f43af78978d76ff8b1a17
patelseth <patelseth@myvuw.ac.nz>
2020-09-15 18:24:18 +1200

    Created a method to scale images Finished createComponents method Created wall and free tiles

--- src/nz/ac/vuw/ecs/swen225/gp20/render/Canvas.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/Canvas.java
@@ -1,36 +1,53 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
 
-import nz.ac.vuw.ecs.swen225.gp20.maze.Player;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Canvas extends JPanel {
-
     private static final int VIEW_SIZE = 9;
     private static final int TILE_SIZE = 10;
-    private final Tile[][] tiles;
-    private final JLabel[][] components; // the 9x9 focus area
-    private final Player player;
+    private final Maze maze;
+    private final JLabel[][] components;
 
-    public Canvas(Tile[][] tiles, Player player) {
-        this.tiles = tiles;
-        this.player = player;
+    /**
+     * New canvas to render the game
+     *
+     * @param maze the maze to be rendered
+     **/
+    public Canvas(Maze maze) {
+        this.maze = maze;
         components = new JLabel[VIEW_SIZE][VIEW_SIZE];
         setPreferredSize(new Dimension(VIEW_SIZE * TILE_SIZE, VIEW_SIZE * TILE_SIZE));
         setLayout(new GridBagLayout());
+        createComponents();
     }
 
     private void createComponents() {
-        // Tile containing the player should be in row 5, column 5, the centre of the 9x9 grid
+        Tile centre = maze.getChap().getLocation();
+        for (int row = centre.getRow() - 4, y = 0; row <= centre.getRow() + 4; row++, y++) {
+            for (int col = centre.getCol() - 4, x = 0; col <= centre.getCol() + 4; col++, x++) {
+                ImageIcon icon = makeImageIcon(maze.getTiles()[col][row].getImageURl());
+                components[x][y] = new JLabel(scaleImage(icon));
+                add(components[x][y]);
+            }
+        }
+    }
 
 
+    private ImageIcon scaleImage(ImageIcon baseImage) {
+        return new ImageIcon(baseImage.getImage().getScaledInstance(TILE_SIZE, TILE_SIZE, Image.SCALE_SMOOTH));
     }
 
     private static ImageIcon makeImageIcon(String filename) {
         java.net.URL imageURL = Canvas.class.getResource(filename);
-        return imageURL != null ? new ImageIcon(imageURL) : null;
+        ImageIcon icon = null;
+        if (imageURL != null) {
+            icon = new ImageIcon(imageURL);
+        }
+        return icon;
     }
 
 }
