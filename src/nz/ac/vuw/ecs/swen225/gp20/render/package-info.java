cfa0942f60d48d3b7f54b1555819115acd646dbe
patelseth <patelseth@myvuw.ac.nz>
2020-09-20 18:34:53 +1200

    Added package info

--- src/nz/ac/vuw/ecs/swen225/gp20/render/package-info.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/package-info.java
@@ -2,8 +2,8 @@
  * The Renderer module.
  * This module is responsible for drawing the maze onto a canvas, including transitions, animations, focus areas and sound.
  *
- * @since
- * @author
- * @version
+ * @author Seth Patel
+ * @version 0.0
+ * @since 14/09/2020
  */
 package nz.ac.vuw.ecs.swen225.gp20.render;
