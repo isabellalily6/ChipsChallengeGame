882412dfdfbdaf5ea1bf44103410e49139b9607a
Matt <rowlinmatt@vuw.ac.nz>
2020-09-21 21:34:14 +1200

    Adding empty level1.json file and LevelLoader.java

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/persistence/LevelLoader.java
@@ -0,0 +1,12 @@
+package nz.ac.vuw.ecs.swen225.gp20.persistence;
+
+public class LevelLoader {
+	
+	/**
+	 * Reads a JSON file describing the level
+	 * @param level number
+	 */
+	public void load(int level) {
+		
+	}
+}
