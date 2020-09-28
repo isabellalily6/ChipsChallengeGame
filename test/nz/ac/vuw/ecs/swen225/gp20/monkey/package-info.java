f79847013365f2f2edff143ed634fe63ac4ae139
Jack Maclennan <jackwmaclennan@gmail.com>
2020-09-28 16:22:49 +1300

    Moved main MonkeyTesting.java to test/monkey package

--- test/nz/ac/vuw/ecs/swen225/gp20/monkey/package-info.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/monkey/package-info.java
@@ -3,8 +3,8 @@
  * This module can load a game level, and play the game using randomly generated input.
  * The purpose of this module is to detect violations of the game logic
  *
- * @since
- * @author
- * @version
+ * @since 2020/09/14
+ * @author maclenjack
+ * @version 0.0
  */
 package nz.ac.vuw.ecs.swen225.gp20.monkey;
