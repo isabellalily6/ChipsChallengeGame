3c67db9e6d0fb266f09ac775cf7cbe7c60c7bdb3
Matt <rowlinmatt@vuw.ac.nz>
2020-10-21 16:23:25 +1300

    Filled out package-info

--- src/nz/ac/vuw/ecs/swen225/gp20/persistence/package-info.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/persistence/package-info.java
@@ -3,8 +3,8 @@
  * This module is responsible for reading map files and reading/writing files representing the current game state (in JSON format)
  * in order for the player to resume games
  *
- * @since
- * @author
- * @version
+ * @since 14/9/2020
+ * @author Matt Rowling 300487163
+ * @version 1.0
  */
 package nz.ac.vuw.ecs.swen225.gp20.persistence;
